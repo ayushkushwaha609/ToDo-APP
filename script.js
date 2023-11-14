b5bb0418890f58275da70586caf00167ce4f4943
Ayush Kushwaha <73795130+ayushkushwaha609@users.noreply.github.com>
2023-11-14 09:44:49 +0530

    Update script.js
    updated javascript.

--- script.js
+++ script.js
@@ -2,6 +2,31 @@ const form = document.querySelector('form');
 const input = document.querySelector('input[type="text"]');
 const todoList = document.querySelector('.todo-list');
 
+// Function to retrieve tasks from local storage
+function getTasksFromStorage() {
+  return JSON.parse(localStorage.getItem('tasks')) || [];
+}
+
+// Function to update tasks in local storage
+function updateTasksInStorage(tasks) {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+// Function to render tasks from storage
+function renderTasks() {
+  const tasks = getTasksFromStorage();
+  tasks.forEach((taskText) => {
+    const todoItem = document.createElement('li');
+    todoItem.classList.add('todo-item');
+    todoItem.innerHTML = `
+      <span class="task">${taskText}</span>
+      <button class="delete">Delete</button>
+    `;
+    todoList.appendChild(todoItem);
+  });
+}
+
+// Add task to the list and local storage
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   const task = input.value.trim();
@@ -10,17 +35,39 @@ form.addEventListener('submit', (event) => {
     todoItem.classList.add('todo-item');
     todoItem.innerHTML = `
       <span class="task">${task}</span>
-  <button class="delete">Delete</button>
-`;
-todoList.appendChild(todoItem);
-input.value = '';
-}
+      <button class="delete">Delete</button>
+    `;
+    todoList.appendChild(todoItem);
+
+    const tasks = getTasksFromStorage();
+    tasks.push(task);
+    updateTasksInStorage(tasks);
+
+    input.value = '';
+  }
 });
 
+// Handle delete and task completion
 todoList.addEventListener('click', (event) => {
-if (event.target.classList.contains('delete')) {
-event.target.parentNode.remove();
-} else if (event.target.classList.contains('task')) {
-event.target.classList.toggle('completed');
-}
+  if (event.target.classList.contains('delete')) {
+    event.target.parentNode.remove();
+    const tasks = getTasksFromStorage();
+    const taskText = event.target.parentNode.querySelector('.task').textContent;
+    const filteredTasks = tasks.filter((task) => task !== taskText);
+    updateTasksInStorage(filteredTasks);
+  } else if (event.target.classList.contains('task')) {
+    event.target.classList.toggle('completed');
+  }
+});
+
+// On page load, render tasks from local storage
+window.addEventListener('load', renderTasks);
+
+// Save tasks to local storage before closing the tab or browser
+window.addEventListener('beforeunload', () => {
+  const tasks = [];
+  document.querySelectorAll('.task').forEach((task) => {
+    tasks.push(task.textContent);
+  });
+  updateTasksInStorage(tasks);
 });
